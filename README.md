341a05ea068608a1897e26a239c545707b36c8ce
Nick Rycar <rycar@chef.io>
2016-11-30 08:56:00 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple_patcher
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'simple_patcher'
+maintainer 'Nick Rycar'
+maintainer_email 'nrycar@chef.io'
+license 'apachev2'
+description 'Installs/Configures simple_patcher'
+long_description 'Installs/Configures simple_patcher'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/simple_patcher/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/simple_patcher' if respond_to?(:source_url)
